40740cbde99a5fb08984f0d5c12049a2e7b95a90
xxspookybootyxx <meljorda@ucsc.edu>
2024-10-17 23:11:19 -0700

    yay

--- lab4/js/lab.js
+++ lab4/js/lab.js
@@ -1,6 +1,23 @@
-// index.js - purpose and description here
-// Author: Your Name
-// Date:
+// Playing a round of RPS (Rock, Paper, Scissors!)
+// How to do it!
+// Lucy Jordan <meljorda@ucsc.edu>
+// October 2024
+
+//Rock, Paper, Scissors!
+  //Have 2 players 
+  //Pound 1 hand each 3x, synchronized 
+    //On each beat when hands make contact with one another both players say 
+      //Rock, then
+      //Paper, then
+      //Scissors.
+  //On the 4th beat both players each say shoot 
+  //Simultaneously deciding & making the hand motion for either rock, paper, or scissors
+  //Depending on the players’ selected hand motion one player is the winner
+    //Rock beats scissors
+    //Scissors beats paper
+    //Paper beats rock
+
+
 
 // Constants
 
