12b49da58be6c6e5e5bf1f9ef457655aaafbc0f7
xxspookybootyxx <meljorda@ucsc.edu>
2024-10-21 14:25:24 -0700

    js

--- lab5/js/lab.js
+++ lab5/js/lab.js
@@ -1,7 +1,25 @@
-// index.js - purpose and description here
-// Author: Your Name
-// Date:
+/**
+ * Author:    Lucy Jordan
+ * Created:   21.10.2024
+ * License: Public Domain
+ **/
 
+
+// Define Variables
+make = "HarleyDavidson";
+model = "Teri";
+color = "black";
+quirks = "twineBinding";
+year = "2005"
+age = 2024 - year
+
+//Output
+document.writeIn("make: " + make + "<br>");
+document.writeIn("model: " + model + "<br>");
+document.writeIn("color: " + color + "<br>");
+document.writeIn("quirks: " + quirks + "<br>");
+document.writeIn("year: " + year + "<br>");
+document.writeIn("age: " + age + "years<br>");
 // Constants
 
 // Functions
