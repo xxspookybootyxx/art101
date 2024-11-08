f77389fa81fdfbff145a4d3332b4d9161b40ef46
xxspookybootyxx <meljorda@ucsc.edu>
2024-11-07 17:38:49 -0800

    Update lab.js

--- lab8/js/lab.js
+++ lab8/js/lab.js
@@ -1,5 +1,37 @@
-//Lucy Jordan <meljorda@ucsc.edu> 7 November 2024
+// //Lucy Jordan <meljorda@ucsc.edu> 7 November 2024
 
+// var numbers = [1, 2, 3, 4, 5];
+
+// function lucyfunction(x) {
+//     var results = x * x;
+//     return results;
+// }
+
+// console.log(lucyfunction(2));
+
+// var mappedNumbers = numbers.map(lucyfunction);
+// console.log("Mapped Results: ", mappedNumbers);
+
+// var newMappedNumbers = numbers.map(function(x) {
+//     var results = x + 1;
+//     return results;
+// });
+
+// console.log("New Mapped Results: ", newMappedNumbers);
+
+//  // Your map results data
+//  var mapResults = "Your map results here";
+
+//  // Use jQuery to select the element by its ID and set the HTML content
+//  $("#output").html(mapResults);
+
+/*
+Lucy Jordan <meljorda@ucsc.edu>
+7 November 2024
+
+This program demonstrates basic array operations and mapping functions in JavaScript. It includes creating an array of numbers, defining a named function for calculations, testing the function, using map with both named and anonymous functions to operate on array elements, and printing out results using console.log().
+
+*/
 var numbers = [1, 2, 3, 4, 5];
 
 function lucyfunction(x) {
@@ -19,8 +51,8 @@ var newMappedNumbers = numbers.map(function(x) {
 
 console.log("New Mapped Results: ", newMappedNumbers);
 
- // Your map results data
- var mapResults = "Your map results here";
+// Assign mappedNumbers to mapResults
+var mapResults = JSON.stringify(mappedNumbers); // Convert to string
 
- // Use jQuery to select the element by its ID and set the HTML content
- $("#output").html(mapResults);
+// Use jQuery to select the element by its ID and set the HTML content
+$("#output").html(mapResults);
