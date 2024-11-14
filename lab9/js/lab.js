4fe80f23537460568dcf427480734ca3f98040a0
xxspookybootyxx <meljorda@ucsc.edu>
2024-11-14 15:30:40 -0800

    lots

---
+++ lab9/js/lab.js
@@ -0,0 +1,8 @@
+/*
+   lab.js - This simple JavaScript/jQuery script uses buttons to modify some elements on the page
+
+   Requirements: jQuery must be loaded for this script to work.
+
+   Author: Lucy Jordan
+   Date: 2024
+*/
