082934a622a7be3e7f1d5724656b330b582b32cb
xxspookybootyxx <meljorda@ucsc.edu>
2024-11-14 17:44:28 -0800

    Update lab.js

--- lab9/js/lab.js
+++ lab9/js/lab.js
@@ -15,3 +15,21 @@ $("#button-challenge").click(function(){
     // now add (or subtract) the "special" class to the section
     $("#challenge").toggleClass("special");
 });
+
+// add button to problems section
+$("#problems").append("<button id='button-problems'>Make Special</button>");
+
+// add a click listener to the challenge button
+$("#button-problems").click(function(){
+    // now add (or subtract) the "special" class to the section
+    $("#problems").toggleClass("special");
+});
+
+// add button to results section
+$("#results").append("<button id='button-results'>Make Special</button>");
+
+// add a click listener to the challenge button
+$("#button-results").click(function(){
+    // now add (or subtract) the "special" class to the section
+    $("#results").toggleClass("special");
+});
